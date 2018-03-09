fdb11ae4885fef04858438f3d00eda1a74fcc712
fourside <fourside@gmail.com>
2018-03-09 13:57:57 +0900

    add form componentns

--- src/BrowserSpeech.js
+++ src/BrowserSpeech.js
@@ -4,8 +4,11 @@ import ReactDOM from 'react-dom';
 class BrowserSpeech extends React.Component {
   render() {
     return (
-      <Header />
-    )
+      <div>
+        <Header />
+        <Synthesiser />
+      </div>
+    );
   }
 }
 function Header(props) {
@@ -13,6 +16,55 @@ function Header(props) {
     <header><h1>Browser Speech</h1></header>
   );
 }
+
+class Synthesiser extends React.Component {
+  render() {
+    return (
+      <form>
+        Enter message and push play button.
+        <Message />
+        <Range name="Rate"/>
+        <Range name="Pitch"/>
+        <VoiceSelect />
+      </form>
+    );
+  }
+}
+
+class Message extends React.Component {
+  render() {
+    return (
+      <div className="form-group">
+        <label for="message"> Message </label>
+          <textarea id="message" className="form-control" placeholder="type what you want to let me say" />
+      </div>
+    );
+  }
+}
+class Range extends React.Component {
+  render() {
+    const name = this.props.name;
+    return (
+      <div className="form-group">
+        <label for="{name}"> {name} </label>
+        <input type="range" id="{name}" className="form-control" />
+      </div>
+    );
+  }
+}
+class VoiceSelect extends React.Component {
+  render() {
+    return (
+      <div className="form-group">
+        <label for="voice"> Voice </label>
+        <select>
+          <option>voice supported by your browser</option>
+        </select>
+      </div>
+    );
+  }
+}
+
 
 export default function() {
   ReactDOM.render(
