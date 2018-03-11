ff6a55eeed7ab738ce432e3cc6468727cd3ee06b
fourside <fourside@gmail.com>
2018-03-11 16:10:11 +0900

    use error boundary

--- src/components/BrowserSpeech.js
+++ src/components/BrowserSpeech.js
@@ -5,12 +5,28 @@ import React from 'react';
 import Speaker from '../util/Speaker';
 import Synthesiser from './Synthesiser';
 
-export default class BrowserSpeech extends React.Component<{}> {
-  speaker: Speaker = new Speaker();
+type State = {
+  hasError: boolean,
+};
+export default class BrowserSpeech extends React.Component<{}, State> {
+  speaker: Speaker;
+
+  constructor(props: {}) {
+    super(props);
+    this.speaker = new Speaker();
+    this.state = { hasError: false };
+  }
+
+  componentDidCatch(error :Error, info: {}) {
+    this.setState({ hasError: true});
+    console.log(error, info);
+  }
 
   render() {
     let mainElement;
-    if (this.speaker.isSupported()) {
+    if (this.state.hasError) {
+      mainElement = <MyError />;
+    } else if (this.speaker.isSupported()) {
       mainElement = <Synthesiser speaker={this.speaker} />;
     } else {
       mainElement = <NotSupported />;
@@ -34,6 +50,16 @@ function NotSupported(props) {
       <div className="alert alert-warning" role="alert">
         <strong>your browser does not support web speech api.</strong>
         <p>please open this with google chrome or mozilla firefox.</p>
+      </div>
+    </div>
+  );
+}
+function MyError(props) {
+  return (
+    <div className="error">
+      <div className="alert alert-danger" role="alert">
+        <strong>Error</strong>
+        <p>Error occured. please reload your browser (press F5).</p>
       </div>
     </div>
   );
